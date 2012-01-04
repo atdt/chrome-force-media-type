de6b496afcfaa622e5a71e3b38797da963caf9fb
Ori Livneh <ori.livneh@gmail.com>
2012-01-04 02:59:53 -0500

    change name in manifest

--- open_as_media_type.js
+++ open_as_media_type.js
@@ -1,11 +1,27 @@
+/*!
+ * Open as Media Type Chrome Extension
+ *
+ * Copyright 2011, Ori Livneh
+ * Licensed under the BSD license; see LICENSE for more details.
+ */
+
 /*jslint maxerr: 50, maxlen: 80, indent: 4 */
 /*global chrome: true */
+
 (function () {
     "use strict";
     var headers_received = chrome.webRequest.onHeadersReceived,
-        target_tab = 0,   // tab where request will be opened
-        target_url = '',  // url to be opened
-        patch_headers = function (mimetype) {
+        media_types = [
+            // Handy list of media types:
+            // http://en.wikipedia.org/wiki/Internet_media_type
+            // TODO: make this customizable
+            'text/html',
+            'text/plain'
+        ],
+        parent_id = 0,    // id of topmost context-menu item
+        target_tab = 0,
+        target_url = '',
+        patch_headers = function (media_type) {
             var callback = function (details) {
                 var headers = details.responseHeaders,
                     is_redirect = false;
@@ -13,26 +29,17 @@
                 if (details.tabId === target_tab) {
                     // this is the request we're meant to intercept; go ahead
                     // and patch headers
-                    console.log(headers);
                     headers.forEach(function (header) {
                         switch (header.name.toLowerCase()) {
                         case 'content-type':
-                            header.value = mimetype;
+                            header.value = media_type;
                             break;
                         case 'content-disposition':
                             // see <http://tools.ietf.org/html/rfc2183>
                             header.value = 'inline';
                             break;
-                        case 'status':
-                            // handle redirects
-                            if (/^30[1237]/.test(header.value)) {
-                                console.log("redirect!");
-                                is_redirect = true;
-                            }
-                            break;
                         case 'location':
                             is_redirect = true;
-                            console.log("redirect (via location)!");
                             target_url = header.value;
                             break;
                         }
@@ -45,7 +52,7 @@
                     // new url as the url filter
                     if (is_redirect) {
                         headers_received.addListener(
-                            patch_headers(mimetype),
+                            patch_headers(media_type),
                             {urls: [target_url]},
                             ['blocking', 'responseHeaders']
                         );
@@ -56,13 +63,13 @@
             };
             return callback;
         },
-        click_handler = function (mimetype) {
+        click_handler = function (media_type) {
             var handler = function (info) {
                 // open the requested url in a new tab
                 target_url = info.linkUrl;
 
                 headers_received.addListener(
-                    patch_headers(mimetype),
+                    patch_headers(media_type),
                     {urls: [target_url]},
                     ['blocking', 'responseHeaders']
                 );
@@ -77,9 +84,17 @@
     //
     // context menu items
     // 
-    chrome.contextMenus.create({
-        title: 'Open Link as text/html',
-        contexts: ['link'],
-        onclick: click_handler("text/html")
+    parent_id = chrome.contextMenus.create({
+        title: "Open as media type\u2026",
+        contexts: ['link']
+    });
+
+    media_types.forEach(function (media_type) {
+        chrome.contextMenus.create({
+            title: media_type,
+            contexts: ['link'],
+            parentId: parent_id,
+            onclick: click_handler(media_type)
+        });
     });
 }());
